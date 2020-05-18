53ea8afb0e230e8d04ff33b29fac2a74287c2a16
ruangroc <ruangroc@oregonstate.edu>
2020-05-17 21:46:40 -0700

    revised the queries so now tables are ables to be imported into the class database server

--- create.sql
+++ create.sql
@@ -1,7 +1,5 @@
 /* Create an inventory table if one doesn't already exist.*/
-
 DROP TABLE IF EXISTS `Inventory`;
-
 CREATE TABLE `Inventory` (
 	`PLU` int(11) NOT NULL AUTO_INCREMENT,
 	`Name` varchar(255) NOT NULL,
@@ -11,22 +9,52 @@ CREATE TABLE `Inventory` (
 	PRIMARY KEY (`PLU`)
 );
 
-
 /* Insert values into the Inventory table*/
-
 LOCK TABLES `Inventory` WRITE;
-
-INSERT INTO `Inventory` VALUES (0, 'Organic Carrots', 'Price per Bundle of 4', 2.47, 9), 
-								(0, 'Red Potatoes', 'Price per 5lb bag', 5.86, 25),
-								(0, 'Fresh Peaches', 'Price per peach', 0.79, 54);
+INSERT INTO `Inventory` VALUES  (0, 'Organic Carrots', 'Price per Bundle of 4', 2.47, 9), 
+								(1, 'Red Potatoes', 'Price per 5lb bag', 5.86, 25),
+								(2, 'Fresh Peaches', 'Price per peach', 0.79, 54);
 UNLOCK TABLES;
 
 
+/* Create an Customers table if one doesn't already exist.*/
+DROP TABLE IF EXISTS `Customers`;
+CREATE TABLE `Customers` (
+	`CustomerID` int(11) NOT NULL AUTO_INCREMENT,
+	`Name` varchar(255), 
+	`PhoneNumber` varchar(255),
+	`RewardsPts` int(11),
+	PRIMARY KEY (`CustomerID`)
+);
 
-/* Create an orders table if one doesn't already exist.*/
+/* Insert values into the Customers table */
+LOCK TABLES `Customers` WRITE;
+INSERT INTO `Customers` VALUES  (0, 'Toph', '123-456-7890', 100), 
+								(1, 'Katara', '987-654-3210', 300), 
+								(2, 'Zuko', '999-888-7777', 250);
+UNLOCK TABLES;
 
+/* Create an Employees table if one doesn't already exist.*/
+DROP TABLE IF EXISTS `Employees`;
+CREATE TABLE `Employees` (
+	`EmployeeID` int(11) NOT NULL AUTO_INCREMENT,
+	`Name` varchar(255), 
+	`HourlyWage` decimal(6,2),
+	`Responsibilities` varchar(255),
+	`SickDays` int(11),
+	PRIMARY KEY (`EmployeeID`)
+);
+
+/* Insert values into the Employees table */
+LOCK TABLES `Employees` WRITE;
+INSERT INTO `Employees` VALUES  (1, 'Will', 15, 'Restock shelves', 4), 
+								(2, 'Tessa', 15, 'Cashier', 6), 
+								(3, 'Jem', 15, 'Customer service', 5);
+UNLOCK TABLES;
+
+
+/* Create an Orders table if one doesn't already exist.*/
 DROP TABLE IF EXISTS `Orders`;
-
 CREATE TABLE `Orders` (
 	`OrderID` int(11) NOT NULL AUTO_INCREMENT,
 	`CustomerID` int(11),
@@ -40,96 +68,36 @@ CREATE TABLE `Orders` (
 	ON DELETE SET NULL
 );
 
-
-
 /* Insert values into the Orders table*/
-
 LOCK TABLES `Orders` WRITE;
-
-INSERT INTO `Orders` VALUES (0, 1, 1), (0, 1, 3), (0, 2, 1);
-
+INSERT INTO `Orders` VALUES (0, 0, 1), (1, 1, 2), (2, 2, 3);
 UNLOCK TABLES;
 
 
 /* Create an OrderItems table if one doesn't already exist.*/
-
 DROP TABLE IF EXISTS `OrderItems`;
-
 CREATE TABLE `OrderItems` (
 	`OrderItemID` int(11) NOT NULL AUTO_INCREMENT,
 	`Quantity` int(11) NOT NULL,
-	`OrderID` int(11) NOT NULL,
+	`OrderID` int(11),
 	`PLU` int(11),
 	PRIMARY KEY (`OrderItemID`),
 	CONSTRAINT `fk_oid`
-	FOREIGN KEY(`OrderID`) REFERENCES Orders(`OrderID`)
-	ON DELETE CASCADE,
+	FOREIGN KEY (`OrderID`) REFERENCES Orders(`OrderID`)
+	ON DELETE SET NULL,
 	CONSTRAINT `fk_plu`
-	FOREIGN KEY(`PLU`) REFERENCES Inventory(`PLU`)
+	FOREIGN KEY (`PLU`) REFERENCES Inventory(`PLU`)
 	ON DELETE SET NULL
 );
 
-
 /* Insert values into the Orders table*/
-
 LOCK TABLES `OrderItems` WRITE;
-
-INSERT INTO `OrderItems` VALUES (0, 4, 1, 2), (0, 6, 3, 3), (0, 1, 2, 1);
-
-UNLOCK TABLES;
-
-
-/* Create an OrderItems table if one doesn't already exist.*/
-
-DROP TABLE IF EXISTS `Customers`;
-
-CREATE TABLE `Customers` (
-	`CustomerID` int(11) NOT NULL AUTO_INCREMENT,
-	`Name` varchar(255), 
-	`PhoneNumber` varchar(255),
-	`RewardsPts` int(11),
-	PRIMARY KEY (`CustomerID`)
-);
-
-/* Insert values into the Customers table */
-
-LOCK TABLES `Customers` WRITE;
-
-INSERT INTO `Customers` VALUES (0, 'Toph', '123-456-7890', 100), 
-								(1, 'Katara', '987-654-3210', 300), 
-								(2, 'Zuko', '999-888-7777', 250);
-
-UNLOCK TABLES;
-
-
-/* Create an Employees table if one doesn't already exist.*/
-
-DROP TABLE IF EXISTS `Employees`;
-
-CREATE TABLE `Employees` (
-	`EmployeeID` int(11) NOT NULL AUTO_INCREMENT,
-	`Name` varchar(255), 
-	`HourlyWage` decimal(6,2),
-	`Responsibilities` varchar(255),
-	`SickDays` int(11),
-	PRIMARY KEY (`EmployeeID`)
-);
-
-/* Insert values into the Employees table */
-
-LOCK TABLES `Employees` WRITE;
-
-INSERT INTO `Employees` VALUES (1, 'Will', 15, 'Restock shelves', 4), 
-								(2, 'Tessa', 15, 'Cashier', 6), 
-								(3, 'Jem', 15, 'Customer service', 5);
-
+INSERT INTO `OrderItems` VALUES (0, 4, 1, 2), (1, 6, 0, 0), (2, 1, 2, 1);
 UNLOCK TABLES;
 
 
 /* Create an Shifts table if one doesn't already exist.*/
-
 DROP TABLE IF EXISTS `Shifts`;
-
 CREATE TABLE `Shifts` (
 	`ShiftID` int(11) NOT NULL AUTO_INCREMENT,
 	`Day` varchar(255), 
@@ -139,37 +107,29 @@ CREATE TABLE `Shifts` (
 );
 
 /* Insert values into the Shifts table */
-
 LOCK TABLES `Shifts` WRITE;
-
 INSERT INTO `Shifts` VALUES (1, 'Monday', '08:00:00', '12:00:00'), 
 							(2, 'Tuesday', '10:00:00', '14:00:00'), 
 							(3, 'Wednesday', '14:00:00', '18:00:00');
-
 UNLOCK TABLES;
 
 
 /* Create an EmployeeShifts table if one doesn't already exist.*/
-
 DROP TABLE IF EXISTS `EmployeeShifts`;
-
 CREATE TABLE `EmployeeShifts` (
 	`EmployeeShiftID` int(11) NOT NULL AUTO_INCREMENT,
 	`EmployeeID` int(11),
 	`ShiftID` int(11),
 	PRIMARY KEY (`EmployeeShiftID`),
-	CONSTRAINT `fk_eid`
+	CONSTRAINT `fk_es_eid`
 	FOREIGN KEY (`EmployeeID`) REFERENCES Employees(`EmployeeID`)
 	ON DELETE SET NULL,
-	CONSTRAINT `fk_sid`
+	CONSTRAINT `fk_es_sid`
 	FOREIGN KEY (`ShiftID`) REFERENCES Shifts(`ShiftID`)
-	ON DELETE SET NULL,
+	ON DELETE SET NULL
 );
 
-/* Insert values into the Employees table */
-
+/* Insert values into the EmployeeShifts table */
 LOCK TABLES `EmployeeShifts` WRITE;
-
 INSERT INTO `EmployeeShifts` VALUES (0, 1, 1), (1, 2, 2), (2, 3, 3);
-
 UNLOCK TABLES;
