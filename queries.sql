791f3c98b5e5cfff073523b16d3a86bb967f3132
ruangroc <ruangroc@oregonstate.edu>
2020-05-17 22:45:43 -0700

    Added queries for the customers, employees, and shifts pages

--- queries.sql
+++ queries.sql
@@ -1,6 +1,9 @@
+-----------------------------
+-- IVENTORY
+-----------------------------
+
 /* Display Inventory table on Manage Inventory Page*/
 SELECT `PLU`, `Name`, `Description`, `UnitCost` AS `Unit Cost` FROM Inventory;
-
 
 /* Insert an item into the Inventory table*/
 --Variables PLU, Name, Description, UnitCost, and Quantity
@@ -8,7 +11,6 @@ SELECT `PLU`, `Name`, `Description`, `UnitCost` AS `Unit Cost` FROM Inventory;
 LOCK TABLES `Inventory` WRITE;
 INSERT INTO `Inventory` VALUES (:PLU, :Name, :Description, :UnitCost, :Quantity);
 UNLOCK TABLES;
-
 
 /* Update a row in the Inventory table*/
 --Variables PLU, Name, Description, UnitCost, and Quantity
@@ -24,16 +26,18 @@ WHERE
 	`PLU` = :PLU;
 UNLOCK TABLES;
 
-
 /* Delect a row in the Inventory table*/
 --Variables PLU, Name, Description, UnitCost, and Quantity
 --to be passed to the database from Python/Flask app
 DELETE FROM `Inventory` WHERE `PLU` = :PLU;
 
 
+-----------------------------
+-- ORDERS
+-----------------------------
+
 /* Display Orders table on Manage Orders Page*/
 SELECT `OrderID` AS 'Order ID', `CustomerID` AS 'Rewards ID', `EmployeeID` AS 'Employee ID' FROM Orders;
-
 
 /* Insert an item into the Orders table*/
 --Variables OrderID, EmployeeID, and CustomerID
@@ -41,7 +45,6 @@ SELECT `OrderID` AS 'Order ID', `CustomerID` AS 'Rewards ID', `EmployeeID` AS 'E
 LOCK TABLES `Orders` WRITE;
 INSERT INTO `Orders` VALUES (0, :CustomerID, :EmployeeID);
 UNLOCK TABLES;
-
 
 /* Update a row in the Orders table*/
 --Variables OrderID, EmployeeID, and CustomerID
@@ -55,12 +58,10 @@ WHERE
 	`OrderID` = :OrderID;
 UNLOCK TABLES;
 
-
 /* Delect a row in the Orders table*/
 --Variables OrderID, EmployeeID, and CustomerID
 --to be passed to the database from Python/Flask app
 DELETE FROM `Orders` WHERE `OrderID` = :OrderID;
-
 
 /* Display Order Details table on Manage Orders Page when user searches by OrderID.*/
 --Variable OrderID to be passed to the database from Python/Flask app.
@@ -70,7 +71,6 @@ JOIN OrderItems on OrderItems.PLU = Inventory.PLU
 JOIN Orders on OrderItems.OrderID = Orders.OrderID
 AND Orders.OrderID = :OrderID
 ORDER BY Orders.OrderID DESC;
-
 
 /* Display Order Details table on Manage Orders Page when user searches by CustomerID.*/
 --Variable OrderID to be passed to the database from Python/Flask app.
@@ -82,7 +82,6 @@ JOIN Customers on Orders.CustomerID = Customers.CustomerID
 AND Customers.CustomerID = :CustomerID
 ORDER BY Orders.OrderID DESC;
 
-
 /* Display Order Details table on Manage Orders Page when user searches by CustomerID.*/
 --Variable OrderID to be passed to the database from Python/Flask app.
 SELECT Orders.OrderID, Inventory.PLU, Inventory.Name, Inventory.UnitCost AS `Unit Cost`, OrderItems.Quantity, (Inventory.UnitCost * OrderItems.Quantity) AS `Total`
@@ -93,14 +92,12 @@ JOIN Employees on Orders.EmployeeID = Employees.EmployeeID
 AND Employees.EmployeeID = :EmployeeID
 ORDER BY Orders.OrderID DESC;
 
-
 /* Display Items in Inventory Order and Customer Order Form tables*/
 --Pulls items one at a time from Inventory by searching by PLU or Item Name
 --And stores data in a temporary list on Flask.
 --Variable PLU to be passed to the database from Python/Flask app.
 SELECT `PLU`, `Name`, `Description`, `UnitCost` AS 'Unit Cost' FROM `Inventory`
 WHERE `PLU` = :PLU;
-
 
 /* Create a new row in Orders and new rows in OrderItems when customer submits the Customer Order Form.*/
 --CustomerID, OrderID, EmployeeID, PLU, Item Name, Description and 
@@ -113,5 +110,161 @@ INSERT INTO `OrderItems` VALUES (0, :Quantity, :OrderID, :PLU);
 UNLOCK TABLES;
 
 
-/* Display Customer table on the View and Manage Customer Information page */
+-----------------------------
+-- CUSTOMERS
+-----------------------------
+/* Display the Customers table on the View and Manage Customer Information page */
 SELECT `CustomerID`, `Name`, `PhoneNumber`, `RewardsPts` FROM Customers;
+
+/* Search for Customers by name */
+SELECT `CustomerID`, `Name`, `PhoneNumber`, `RewardsPts` FROM Customers
+WHERE `Name` = :Name;
+
+/* Search for Customers by ID */
+SELECT `CustomerID`, `Name`, `PhoneNumber`, `RewardsPts` FROM Customers
+WHERE `CustomerID` = :CustomerID;
+
+/* Search for Customers by Phone Number */
+SELECT `CustomerID`, `Name`, `PhoneNumber`, `RewardsPts` FROM Customers
+WHERE `PhoneNumber` = :PhoneNumber;
+
+/* Search for Customers by Rewards Points */
+SELECT `CustomerID`, `Name`, `PhoneNumber`, `RewardsPts` FROM Customers
+WHERE `RewardsPts` >= :RewardsLowerBound AND `RewardsPts` <= :RewardsUpperBound;
+
+/* Update a row in the Customers table*/
+--Variables Name, PhoneNumber, and RewardsPts
+--to be passed to the database from Python/Flask app
+LOCK TABLES `Customers` WRITE;
+UPDATE `Customers` 
+SET
+	`Name` = :Name,
+	`PhoneNumber` = :PhoneNumber,
+	`RewardsPts` = :RewardsPts
+WHERE
+	`CustomerID` = :CustomerID;
+UNLOCK TABLES;
+
+/* Delete a row from the Customers table */
+DELETE FROM `Customers` WHERE `CustomerID` = :CustomerID;
+
+
+-----------------------------
+-- EMPLOYEES
+-----------------------------
+
+/* Display the Employees table on the Manage Employees page */
+SELECT `EmployeeID`, `Name`, `HourlyWage`, `Responsibilities`, `SickDays` FROM `Employees`;
+
+/* Search for Employees by name */
+SELECT `EmployeeID`, `Name`, `HourlyWage`, `Responsibilities`, `SickDays` FROM `Employees`
+WHERE `Name` = :Name;
+
+/* Search for Employees by ID */
+SELECT `EmployeeID`, `Name`, `HourlyWage`, `Responsibilities`, `SickDays` FROM `Employees`
+WHERE `EmployeeID` = :EmployeeID;
+
+/* Search for Employees by Responsibilities */
+SELECT `EmployeeID`, `Name`, `HourlyWage`, `Responsibilities`, `SickDays` FROM `Employees`
+WHERE `Responsibilities` = :Responsibilities;
+
+/* Search for Employees by Hourly Wage */
+SELECT `EmployeeID`, `Name`, `HourlyWage`, `Responsibilities`, `SickDays` FROM `Employees`
+WHERE `HourlyWage` = :HourlyWage;
+
+/* Search for Employees by Number of Sick Days */
+SELECT `EmployeeID`, `Name`, `HourlyWage`, `Responsibilities`, `SickDays` FROM `Employees`
+WHERE `SickDays` = :SickDays;
+
+/* Update a row in the Employees table*/
+--Variables Name, HourlyWage, Responsibilities, and SickDays
+--to be passed to the database from Python/Flask app
+LOCK TABLES `Employees` WRITE;
+UPDATE `Employees` 
+SET
+	`Name` = :Name,
+	`HourlyWage` = :HourlyWage,
+	`Responsibilities` = :Responsibilities,
+	`SickDays` = :SickDays
+WHERE
+	`EmployeeID` = :EmployeeID;
+UNLOCK TABLES;
+
+/* Delete a row from the Employees table */
+DELETE FROM `Employees` WHERE `EmployeeID` = :EmployeeID;
+
+/* Display Shifts assigned to selected Employee */
+SELECT Employees.Name, Shifts.ShiftID, Shifts.Day, Shifts.StartTime, Shifts.EndTime
+FROM `Shifts`
+JOIN `EmployeeShifts` ON Shifts.ShiftID = EmployeeShifts.ShiftID
+JOIN `Employees` ON EmployeeShifts.EmployeeID = Employees.EmployeeID
+WHERE Employees.EmployeeID = :EmployeeID;
+
+/* Insert a new Employee into the Employees table*/
+--Variables to be passed to the database from Python/Flask app
+LOCK TABLES `Employees` WRITE;
+INSERT INTO `Employees` (`Name`, `HourlyWage`, `Responsibilities`, `SickDays`) 
+VALUES (:Name, :HourlyWage, :Responsibilities, :SickDays);
+UNLOCK TABLES;
+
+
+-----------------------------
+-- SHIFTS
+-----------------------------
+
+/* Display the Shifts table on the Manage Shifts page */
+SELECT `ShiftID`, `Day`, `StartTime`, `EndTime` FROM `Shifts`;
+
+/* Search for Shifts by ID */
+SELECT `ShiftID`, `Day`, `StartTime`, `EndTime` FROM `Shifts`
+WHERE `ShiftID` = :ShiftID;
+
+/* Search for Shifts by day */
+SELECT `ShiftID`, `Day`, `StartTime`, `EndTime` FROM `Shifts`
+WHERE `Day` = :Day;
+
+/* Search for Shifts by Start Time */
+SELECT `ShiftID`, `Day`, `StartTime`, `EndTime` FROM `Shifts`
+WHERE `StartTime` = :StartTime;
+
+/* Search for Shifts by Start Time */
+SELECT `ShiftID`, `Day`, `StartTime`, `EndTime` FROM `Shifts`
+WHERE `StartTime` = :StartTime;
+
+/* Update a row in the Shifts table*/
+--Variables to be passed to the database from Python/Flask app
+LOCK TABLES `Shifts` WRITE;
+UPDATE `Shifts` 
+SET
+	`Day` = :Day,
+	`StartTime` = :StartTime,
+	`EndTime` = :EndTime,
+WHERE
+	`ShiftID` = :ShiftID;
+UNLOCK TABLES;
+
+/* Delete a row from the Shifts table */
+DELETE FROM `Shifts` WHERE `ShiftID` = :ShiftID;
+
+/* Display Employees assigned to selected Shift */
+SELECT Employees.Name, Shifts.ShiftID, Shifts.Day, Shifts.StartTime, Shifts.EndTime
+FROM `Shifts`
+JOIN `EmployeeShifts` ON Shifts.ShiftID = EmployeeShifts.ShiftID
+JOIN `Employees` ON EmployeeShifts.EmployeeID = Employees.EmployeeID
+WHERE Shifts.ShiftID = :ShiftID;
+
+/* Insert a new Shift into the Shifts table*/
+--Variables to be passed to the database from Python/Flask app
+LOCK TABLES `Shifts` WRITE;
+INSERT INTO `Shifts` (`Day`, `StartTime`, `EndTime`) 
+VALUES (:Day, :StartTime, :EndTime);
+UNLOCK TABLES;
+
+/* Assign a Shift to an Employee */
+-- Variables ShiftID and EmployeeID to be passed to
+-- the database from Python/Flask app.
+-- Equivalent to inserting new row into the EmployeeShifts table.
+LOCK TABLES `EmployeeShifts` WRITE;
+INSERT INTO `EmployeeShifts` (`EmployeeID`, `ShiftID`) 
+VALUES (:EmployeeID, :ShiftID);
+UNLOCK TABLES;
