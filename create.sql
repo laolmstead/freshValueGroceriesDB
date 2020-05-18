1334a5894a1ead6780642c1e2c2c49711ae855cb
ruangroc <ruangroc@oregonstate.edu>
2020-05-17 18:12:24 -0700

    Added statements to create and insert data into Customers, Employees, Shifts, and EmployeeShifts

--- create.sql
+++ create.sql
@@ -29,15 +29,15 @@ DROP TABLE IF EXISTS `Orders`;
 
 CREATE TABLE `Orders` (
 	`OrderID` int(11) NOT NULL AUTO_INCREMENT,
-	/*`CustomerID` int(11),
-	`EmployeeID` int(11),*/
-	PRIMARY KEY (`OrderID`)/*,
+	`CustomerID` int(11),
+	`EmployeeID` int(11),
+	PRIMARY KEY (`OrderID`),
 	CONSTRAINT `fk_cid`
 	FOREIGN KEY (`CustomerID`) REFERENCES Customers(`CustomerID`)
 	ON DELETE SET NULL,
 	CONSTRAINT `fk_eid`
 	FOREIGN KEY (`EmployeeID`) REFERENCES Employees(`EmployeeID`)
-	ON DELETE SET NULL*/
+	ON DELETE SET NULL
 );
 
 
@@ -46,12 +46,12 @@ CREATE TABLE `Orders` (
 
 LOCK TABLES `Orders` WRITE;
 
-INSERT INTO `Orders` VALUES (0/*, 1, 1*/), (0/*, 1, 3*/), (0/*, 2, 1*/);
+INSERT INTO `Orders` VALUES (0, 1, 1), (0, 1, 3), (0, 2, 1);
 
 UNLOCK TABLES;
 
 
-/* Create an OrderItems table if one doesn't already exit.*/
+/* Create an OrderItems table if one doesn't already exist.*/
 
 DROP TABLE IF EXISTS `OrderItems`;
 
@@ -75,5 +75,101 @@ CREATE TABLE `OrderItems` (
 LOCK TABLES `OrderItems` WRITE;
 
 INSERT INTO `OrderItems` VALUES (0, 4, 1, 2), (0, 6, 3, 3), (0, 1, 2, 1);
+
+UNLOCK TABLES;
+
+
+/* Create an OrderItems table if one doesn't already exist.*/
+
+DROP TABLE IF EXISTS `Customers`;
+
+CREATE TABLE `Customers` (
+	`CustomerID` int(11) NOT NULL AUTO_INCREMENT,
+	`Name` varchar(255), 
+	`PhoneNumber` varchar(255),
+	`RewardsPts` int(11),
+	PRIMARY KEY (`CustomerID`)
+);
+
+/* Insert values into the Customers table */
+
+LOCK TABLES `Customers` WRITE;
+
+INSERT INTO `Customers` VALUES (0, 'Toph', '123-456-7890', 100), 
+								(1, 'Katara', '987-654-3210', 300), 
+								(2, 'Zuko', '999-888-7777', 250);
+
+UNLOCK TABLES;
+
+
+/* Create an Employees table if one doesn't already exist.*/
+
+DROP TABLE IF EXISTS `Employees`;
+
+CREATE TABLE `Employees` (
+	`EmployeeID` int(11) NOT NULL AUTO_INCREMENT,
+	`Name` varchar(255), 
+	`HourlyWage` decimal(6,2),
+	`Responsibilities` varchar(255),
+	`SickDays` int(11),
+	PRIMARY KEY (`EmployeeID`)
+);
+
+/* Insert values into the Employees table */
+
+LOCK TABLES `Employees` WRITE;
+
+INSERT INTO `Employees` VALUES (1, 'Will', 15, 'Restock shelves', 4), 
+								(2, 'Tessa', 15, 'Cashier', 6), 
+								(3, 'Jem', 15, 'Customer service', 5);
+
+UNLOCK TABLES;
+
+
+/* Create an Shifts table if one doesn't already exist.*/
+
+DROP TABLE IF EXISTS `Shifts`;
+
+CREATE TABLE `Shifts` (
+	`ShiftID` int(11) NOT NULL AUTO_INCREMENT,
+	`Day` varchar(255), 
+	`StartTime` time(0),
+	`EndTime` time(0),
+	PRIMARY KEY (`ShiftID`)
+);
+
+/* Insert values into the Shifts table */
+
+LOCK TABLES `Shifts` WRITE;
+
+INSERT INTO `Shifts` VALUES (1, 'Monday', '08:00:00', '12:00:00'), 
+							(2, 'Tuesday', '10:00:00', '14:00:00'), 
+							(3, 'Wednesday', '14:00:00', '18:00:00');
+
+UNLOCK TABLES;
+
+
+/* Create an EmployeeShifts table if one doesn't already exist.*/
+
+DROP TABLE IF EXISTS `EmployeeShifts`;
+
+CREATE TABLE `EmployeeShifts` (
+	`EmployeeShiftID` int(11) NOT NULL AUTO_INCREMENT,
+	`EmployeeID` int(11),
+	`ShiftID` int(11),
+	PRIMARY KEY (`EmployeeShiftID`),
+	CONSTRAINT `fk_eid`
+	FOREIGN KEY (`EmployeeID`) REFERENCES Employees(`EmployeeID`)
+	ON DELETE SET NULL,
+	CONSTRAINT `fk_sid`
+	FOREIGN KEY (`ShiftID`) REFERENCES Shifts(`ShiftID`)
+	ON DELETE SET NULL,
+);
+
+/* Insert values into the Employees table */
+
+LOCK TABLES `EmployeeShifts` WRITE;
+
+INSERT INTO `EmployeeShifts` VALUES (0, 1, 1), (1, 2, 2), (2, 3, 3);
 
 UNLOCK TABLES;
