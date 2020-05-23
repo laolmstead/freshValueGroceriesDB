f306919e9c781f49be59ed23c0f6ed6c04f72ec5
ruangroc <ruangroc@oregonstate.edu>
2020-05-22 18:16:39 -0700

    Started templatizing the customer, index, shifts, and employees pages so the nav bar doesn't have to be updated everywhere if/when we need to change it

--- app_package/routes.py
+++ app_package/routes.py
@@ -5,3 +5,32 @@ from app_package import app
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/customers')
+def customers_page():
+    return render_template('customers.html')
+
+@app.route('/orders')
+def orders_page():
+    return render_template('orders.html')
+
+@app.route('/customerOrder')
+def customerOrder_page():
+    return render_template('customerOrder.html')
+
+@app.route('/employees')
+def employees_page():
+    return render_template('employees.html')
+
+@app.route('/shifts')
+def shifts_page():
+    return render_template('shifts.html')
+
+@app.route('/inventory')
+def inventory_page():
+    return render_template('inventory.html')
+
+@app.route('/inventoryOrder')
+def inventoryOrder_page():
+    return render_template('inventoryOrder.html')
+
