282ca41bcd58d0b990f01ef408e23cd9003dcd09
Lindsey Olmstead <laolmstead19@gmail.com>
2020-05-31 15:49:49 -0400

    Implement Update/Delete functionality for Place an Order page

--- app_package/static/js/customerOrder.js
+++ app_package/static/js/customerOrder.js
@@ -82,7 +82,6 @@ function inventoryDropdown() {
                     var select = document.getElementById("inventoryDropdown");
                     var option = document.createElement("option");
                     select.appendChild(option);
-                    console.log(value);
                     option.text = value;
                     select.add(option);
                 }
@@ -205,7 +204,7 @@ function addToInvForm(orderItem, quantItem){
 
     // Add Cost
     var costCell = document.createElement("TD");
-    var costText = document.createTextNode(orderItem[0][3] * quantItem);
+    var costText = document.createTextNode((orderItem[0][3] * quantItem).toFixed(2));
     costCell.appendChild(costText);
     newRow.appendChild(costCell);
 
@@ -215,7 +214,58 @@ function addToInvForm(orderItem, quantItem){
     updateButton.setAttribute("type", "button");
     updateButton.classList.add("btn", "btn-success");
     updateButton.innerText = "Update";
+    updateButton.style.display = 'block';
     updateTD.appendChild(updateButton);
+    //Attach Update function
+    updateButton.onclick = function(updateButton) {
+        var tr = updateButton.path[2];
+        var currentRow = tr.cells;
+
+        // Make Quantity cell editable.
+        currentRow.item(4).contentEditable = "true";
+        currentRow.item(4).classList.add("form-cell-style");
+
+        // Hide Update Button and Show Submit button.
+        var ub = currentRow.item(6).childNodes[0];
+        ub.style.display = 'none';
+        var sb = currentRow.item(6).childNodes[1];
+        sb.style.display = 'block';
+
+        // Hide Delete Button.
+        var db = currentRow.item(7).childNodes[0];
+        db.style.display = 'none';
+    }
+
+    // Add Submit Button
+    var submitButton = document.createElement('button');
+    submitButton.setAttribute("type", "button");
+    submitButton.classList.add("btn", "btn-success");
+    submitButton.innerText = "Submit";
+    submitButton.style.display = 'none';
+    updateTD.appendChild(submitButton);
+    // Attach Submit Button
+    submitButton.onclick = function(submitButton) {
+        var tr = submitButton.path[2];
+        var currentRow = tr.cells;
+
+        // Make Quantity cell uneditable.
+        currentRow.item(4).contentEditable = "false";
+        currentRow.item(4).classList.remove("form-cell-style");
+
+        // Show Update Button and Hide Submit button.
+        var ub = currentRow.item(6).childNodes[0];
+        ub.style.display = 'block';
+        var sb = currentRow.item(6).childNodes[1];
+        sb.style.display = 'none';
+
+        // Show Delete Button.
+        var db = currentRow.item(7).childNodes[0];
+        db.style.display = 'block';
+44
+        // Update cost
+        var costCell = currentRow.item(5);
+        costCell.innerHTML = Number(currentRow.item(4).innerHTML) * Number(currentRow.item(3).innerHTML);
+    }
     newRow.appendChild(updateTD);
 
     // Add Delete Button
@@ -224,7 +274,16 @@ function addToInvForm(orderItem, quantItem){
     deleteButton.setAttribute("type", "button");
     deleteButton.classList.add("btn", "btn-danger");
     deleteButton.innerText = "Delete";
+    deleteButton.style.display = 'block';
     deleteTD.appendChild(deleteButton);
+    // Attach Delete function
+    deleteButton.onclick = function(deleteButton) {
+        var table = document.getElementById("orderTable");
+        var tr = deleteButton.path[2];
+        var row = tr.rowIndex;
+        table.deleteRow(row);
+    };
+
     newRow.appendChild(deleteTD);
 }
 
